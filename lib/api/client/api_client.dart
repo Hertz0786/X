ea323690697a62cf4da59406ab1023f9895fc8eb
Chessy <ilovepakpak@gmail.com>
2025-04-16 22:36:21 +0700

    refactor

---
+++ lib/api/client/api_client.dart
@@ -0,0 +1,55 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+typedef FromJson<T> = T Function(Map<String, dynamic> json);
+
+class ApiClient {
+  final String baseUrl = "http://localhost:8000";
+
+  ApiClient();
+
+  Future<T> get<T>(String path, {required FromJson<T> fromJson}) async {
+    final response = await http.get(
+      Uri.parse('$baseUrl$path'),
+      headers: {'Content-Type': 'application/json'},
+    );
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      return fromJson(data);
+    } else {
+      throw Exception('GET thất bại: ${response.body}');
+    }
+  }
+
+  Future<T> post<T>(
+    String path, {
+    required dynamic body,
+    required FromJson<T> fromJson,
+  }) async {
+    final response = await http.post(
+      Uri.parse('$baseUrl$path'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode(body),
+    );
+
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      final data = jsonDecode(response.body);
+      return fromJson(data);
+    } else {
+      throw Exception('POST thất bại: ${response.body}');
+    }
+  }
+
+  Future<void> delete(String path) async {
+    final response = await http.delete(
+      Uri.parse('$baseUrl$path'),
+      headers: {'Content-Type': 'application/json'},
+    );
+
+    if (response.statusCode != 200 && response.statusCode != 204) {
+      throw Exception('DELETE thất bại: ${response.body}');
+    }
+  }
+}
